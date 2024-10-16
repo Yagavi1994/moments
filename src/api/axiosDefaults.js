c65f177b610c8164f344c4903c5a6a7438124c47
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-10-16 12:41:30 +0000

    Add posts.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-api-1-17088e476ea9.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
