1ee070edaa296fd9acf5757b672180890b1ddb75
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-10-16 11:53:21 +0000

    Add navbar links and adjust user experience in it.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm/>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
